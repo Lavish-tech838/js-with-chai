03469fb5463d3b827cdc99e157c8cea395b60a59
Lavish Singhal <74144590+Lavish-tech838@users.noreply.github.com>
2023-10-14 19:03:18 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,35 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Premitive)
+
+let myGitName="lavishsinghal"
+
+let anothername=myGitName
+anothername="javascriptwithcode"
+
+// console.log(myGitName)
+// console.log(anothername)
+
+let userOne = {
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email="lavish@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
